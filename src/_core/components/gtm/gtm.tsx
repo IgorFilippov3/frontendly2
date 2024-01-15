c7b97019f02fd849395309514b3bf00a35f1d3fe
igorfilippov3 <filippovigorz@gmail.com>
2024-01-15 15:46:12 +0100

    add sitemap and robots.txt

--- src/_core/components/gtm/gtm.tsx
+++ src/_core/components/gtm/gtm.tsx
@@ -1,6 +1,7 @@
 import Script from 'next/script';
 
 export const Gtm = () => {
+  if (process.env.NODE_ENV === 'development') return null;
   return (
     <>
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-RMG5K1187W" />
